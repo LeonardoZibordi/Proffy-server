1cf69d6c9c7d7d8f8fa7a8ed49f32c559b32c45d
Leonardo Zibrdi <leonardo.zibordi@hotmail.com>
2020-09-05 15:14:25 -0300

    Commitando TUDO

---
+++ README.md
@@ -0,0 +1,21 @@
+<!-- GET: Buscar ou listar infos
+POST: criar novas infos
+PUT: atualizar infos existentes
+DELETE: deletar um info existente
+
+Corpo (Request Boby): Dados para criação de um registro
+Route Params: Identificar qual recurso eu quero deletar ou atualizar
+Query Params: Paginação, filtros, ordenação  -->
+
+# Funcionalidades
+
+## Conexões
+
+- Rota para listar o total de conexões realizadas;
+- Rota para criar uma nova conexão;
+
+
+## Aulas
+- Rota para criar uma aula;
+- Rota para listar aula;
+  - Filtar por matéria, dia da semana e horário;
---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConectionsController';
+
+const routes = express.Router();
+const classesContoller = new ClassesController();
+const connectionsController = new ConnectionsController
+
+
+routes.get('/classes', classesContoller.index);
+routes.post('/classes', classesContoller.create);
+
+routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.create)
+
+export default routes;
